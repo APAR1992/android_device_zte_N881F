6252d924ef9f4ec02d405bef2976c6e0eb70d9da
蓝鹏 <focus.lau@gmail.com>
2013-02-20 15:26:39 +0800

    added product packages

--- device_N881F.mk
+++ device_N881F.mk
@@ -17,8 +17,31 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+# Graphics
+PRODUCT_PACKAGES := \
+        gralloc.msm7627a \
+        hwcomposer.msm7627a \
+        copybit.msm7627a
+
+# Camera
+PRODUCT_PACKAGES += \
+        camera.msm7627a
+
+# Sensor
+PRODUCT_PACKAGES += \
+        sensors.msm7627a
+
+# Light
+PRODUCT_PACKAGES += \
+        lights.msm7627a
+
+# Audio
+PRODUCT_PACKAGES += \
+        audio_policy.msm7627a \
+		audio.primary.msm7627a
+
 $(call inherit-product, build/target/product/full.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
 PRODUCT_NAME := full_N881F
 PRODUCT_DEVICE := N881F
